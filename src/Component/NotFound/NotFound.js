5fcbfde867ef8c753a22d92a4b494b014fa1cff5
jibonghoshbd <jibonghosh1510@gmail.comm>
2021-10-01 08:04:56 +0600

    added new component

---
+++ src/Component/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>Page not found--404. Don't askd me the page.It's very danger</h2>
+        </div>
+    );
+};
+
+export default NotFound;
---
+++ src/Component/OrderRevew/OrderRevew.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderRevew = () => {
+    return (
+        <div>
+            <h2>This Is Order Revew</h2>
+        </div>
+    );
+};
+
+export default OrderRevew;
